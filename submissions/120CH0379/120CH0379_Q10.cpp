73543d0042c32f021d47ca259eb7f6ad92435254
Ancoco <88350684+Ancoco@users.noreply.github.com>
2021-08-18 17:02:38 +0530

    Update 120CH0379_Q10.cpp

--- submissions/120CH0379/120CH0379_Q10.cpp
+++ submissions/120CH0379/120CH0379_Q10.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) 
+      int search(vector<int>& a, int t) 
     {
-        int k=-1;
-        for(int i=0;i<nums.size();i++)
+        int n=a.size();
+        int s=0,e=n-1;
+        while(s<=e)
         {
-            if(nums[i]==target)
-                k=i;
+            int m=(s+e)/2;
+            if(a[m]==t)
+            {
+                return m;
+            }
+            else if(a[m]>=a[s])
+            {
+                if(a[m]>=t && a[s]<=t)
+                    e=m-1;
+                else
+                    s=m+1;
+            }
+            else 
+            {
+                if(a[m]<=t && a[e]>=t)
+                    s=m+1;
+                else
+                   e=m-1;
+            }
         }
-        return k;
+        return -1;
     }
 };
